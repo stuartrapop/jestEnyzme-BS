af10967a9f4016398f922a64c53d8b01e6b0300d
stuartrapop <stuartrapop@gmail.com>
2021-01-06 14:24:24 +0100

    added more tests

--- src/Congrats.js
+++ src/Congrats.js
@@ -13,7 +13,7 @@ const Congrats =  (props) => {
 
 if(props.success) {
     return(
-      <div data-test="component-congrats"> 
+      <div data-test="component-congrats" className="alert alert-success"> 
         
          <span data-test="congrats-message">
             Congratulations! You guessed the word
--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -13,13 +13,13 @@ const GuessedWords = (props) => {
      );
     } else {
       const guessedWordsRows = props.guessedWords.map(({guessedWord, letterMatchCount}, index) => {
-        return ( <tr key={index} data-test="guessed-word"><td>guessedWord</td><td>letterMatchCount</td></tr> );
+        return ( <tr key={index} data-test="guessed-word"><td>{guessedWord}</td><td>{letterMatchCount}</td></tr> );
       });
       contents = (
         <div data-test="guessed-words">
           <h3>Guessed Words</h3>
-          <table>
-            <thead>
+          <table className="table table-sm">
+            <thead className="thead-light">
               <tr><th>Guess</th><th>Matching Letters</th></tr>
             </thead>
             <tbody>
