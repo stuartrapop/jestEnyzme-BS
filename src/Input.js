654e0f73a3eb7473c22210158dcb8b4ae9c79503
stuartrapop <stuartrapop@gmail.com>
2021-01-07 10:56:44 +0100

    added input form success = false

--- src/Input.js
+++ src/Input.js
@@ -5,18 +5,39 @@ import { connect } from 'react-redux';
 class Input extends Component {
 
   render() {
+    const contents =  this.props.success
+      ? null
+      : (
+        <form 
+        className="form-inline"
+        >
+          <input 
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            type="text"
+            placeholder="enter guess"
+          />
+          <button 
+            data-test="submit-button"
+            className="btn btn-primary mb-2"
+            type="submit"
+          >Submit
+          </button>
+        </form>
+
+      );
+
+
     return (
-    <div>
-      <button>
-        Click Me
-      </button>
+    <div data-test="component-input">
+      { contents }
     </div>
     );
   }
 };
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
