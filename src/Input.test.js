cd110be5b116a514163518352d6bde520c1a59d6
stuartrapop <stuartrapop@gmail.com>
2021-01-07 11:07:41 +0100

    added success = true for input form

--- src/Input.test.js
+++ src/Input.test.js
@@ -27,7 +27,6 @@ describe('render', () => {
     test('renders component without error', () => {
       const component = findByTestAttr(wrapper, 'component-input');
       expect(component.length).toBe(1);
-
     });
     test('renders input box', () => {
       const inputBox = findByTestAttr(wrapper, 'input-box');
@@ -37,21 +36,24 @@ describe('render', () => {
       const submitButton = findByTestAttr(wrapper, 'submit-button');
       expect(submitButton.length).toBe(1);
     });
-
   });
   describe('word has  been guessed',() => {
+    let wrapper;
+    const initialState = {success: true};
+    wrapper = setup(initialState);
     test('renders component without error', () => {
-
-    });
+      const component = findByTestAttr(wrapper,'component-input');
+      expect(component.length).toBe(1);
+    }); 
     test('does not renders input box', () => {
-
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
     });
     test('does not renders submit button', () => {
-
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(0);
     });
-
   });
-
 });
 
 describe('update state', () => {
